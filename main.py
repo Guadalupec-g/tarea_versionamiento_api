ccb6815ea8318411ae3085391beacdd3ed68ffcd
Guadalupec-g <84210876+Guadalupec-g@users.noreply.github.com>
2025-02-26 12:10:51 -0600

    Agregar endpoint POST para crear usuarios

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+users = []
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
 
 @app.get("/")
 def read_root():
@@ -10,3 +18,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# ✅ Nuevo endpoint POST para guardar usuarios
+@app.post("/users/")
+def add_user(user: User):
+    users.append(user)
+    return {"message": "Usuario agregado correctamente", "user": user}
